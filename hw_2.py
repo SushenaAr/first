f4a90218017b7af525652c62a54ac042ad41d9d8
Sergey Mironov <afgjz.06@gmail.com>
2022-08-18 16:23:55 +0300

    change code

--- hw_2.py
+++ hw_2.py
@@ -33,42 +33,37 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 def files():
     with open('1.txt', encoding= 'utf-8') as f1, open('2.txt', encoding= 'utf-8') as f2, open('fw.txt', 'a', encoding= 'utf-8') as fw:
-        all_text= []
-        all_text_sorted= []
 
-        #т.к я знаю кол-во и названия файлов, могу обойтись без цикла
+        #т.к я знаю кол-во и названия файлов, могу обойтись без цикла, так с маленьким объемом мне кажется,
+        # будет быстрее
         count_1= 0
         text_1=[]
         for line in f1:
             count_1+= 1
-            text.append(f'строка номер {count_1} файла номер 1')
-
-        all_text.append(['1.txt', count_1, text_1])
+            text_1.append(f'строка номер {count_1} файла номер 1')
 
         count_2= 0
         text_2= []
         for line in f2:
             count_2+= 1
             text_2.append(f'строка номер {count_2} файла номер 2')
-        all_text.append(['2.txt', count_2, text_2])
 
-        #сравниваю, тут я даже не представляю сравнение по другому и если файлов будет больше двух, и дедлайн уже горит во всю
-        a= all_text[0][1]> all_text[1][1]
+        #сравниваю, тут я даже не представляю сравнение по другому и если файлов будет больше двух
+        a= count_1> count_2
         if a == True:
-            all_text_sorted.append(all_text[1])
-            all_text_sorted.append(all_text[0])
+            fw.write(f"1.txt\n{count_1}\n")
+            for a in text_1:
+                fw.write(f'{a}\n')
+            fw.write(f"2.txt\n{count_2}\n")
+            for b in text_2:
+                fw.write(f'{b}\n')
         else:
-            all_text_sorted.append(all_text[0])
-            all_text_sorted.append(all_text[1])
-        pprint(all_text_sorted)
-
-
-
-
-
-
-
-
+            fw.write(f"2.txt\n{count_2}\n")
+            for b in text_2:
+                fw.write(f'{b}\n')
+            fw.write(f"1.txt\n{count_1}\n")
+            for a in text_1:
+                fw.write(f'{a}\n')
 
 files()
 
