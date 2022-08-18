370f317f67c7c95cbc174c66fbfb49775b1242a0
Sergey Mironov <afgjz.06@gmail.com>
2022-08-18 14:12:04 +0300

    change hw_2.py

--- hw_2.py
+++ hw_2.py
@@ -31,19 +31,46 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 
 
-def files(files):
-    path= "C:\e"
-    a= os.listdir(path)
-    for file in files:
-        if file in a:
-            with open(file, encoding= 'utf-8') as f, open('fw.txt', 'a', encoding= 'utf-8') as fw:
-                count= 0
-                list_files= []
-                for line in f:
-                    count +=1
-                    list_files.append(f"Строка номер {count} файла номер {file}\n")
-                fw.write(f'\n{file}\n{count}\n{list_files}')
-files(['1.txt','2.txt','3.txt'])
+def files():
+    with open('1.txt', encoding= 'utf-8') as f1, open('2.txt', encoding= 'utf-8') as f2, open('fw.txt', 'a', encoding= 'utf-8') as fw:
+        all_text= []
+        all_text_sorted= []
+
+        #т.к я знаю кол-во и названия файлов, могу обойтись без цикла
+        count_1= 0
+        text_1=[]
+        for line in f1:
+            count_1+= 1
+            text.append(f'строка номер {count_1} файла номер 1')
+
+        all_text.append(['1.txt', count_1, text_1])
+
+        count_2= 0
+        text_2= []
+        for line in f2:
+            count_2+= 1
+            text_2.append(f'строка номер {count_2} файла номер 2')
+        all_text.append(['2.txt', count_2, text_2])
+
+        #сравниваю, тут я даже не представляю сравнение по другому и если файлов будет больше двух, и дедлайн уже горит во всю
+        a= all_text[0][1]> all_text[1][1]
+        if a == True:
+            all_text_sorted.append(all_text[1])
+            all_text_sorted.append(all_text[0])
+        else:
+            all_text_sorted.append(all_text[0])
+            all_text_sorted.append(all_text[1])
+        pprint(all_text_sorted)
+
+
+
+
+
+
+
+
+
+files()
 
 
 
